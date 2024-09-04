2ef57a41e23dbf63e8f34c65271cfc268fe7cc44
jiangyuhui <1225348331@qq.com>
2024-09-04 09:17:42 +0800

    demo

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import { routes } from "../utils/dynamicViews";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/components/layouts/index.vue"),
+      redirect: {
+        path: "/" + routes[0].path,
+      },
+      children: routes,
+    },
+    {
+      path: "/404",
+      component: () => import("@/components/error-page/404.vue"),
+      alias: "/:pathMatch(.*)*",
+    },
+  ],
+});
+
+export default router;
---
+++ src/views/时序动画/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+</script>
+<template>
+  <div>时序动画</div>
+</template>
+<style lang="scss" scoped></style>
